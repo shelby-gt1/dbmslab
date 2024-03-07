326ba65898c28f3a8d3b89178a1273de9507b15b
mrunknown <143517669+shelby-gt1@users.noreply.github.com>
2024-03-07 23:01:23 +0530

    Add files via upload

---
+++ college.sql
@@ -0,0 +1,208 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 06, 2024 at 06:51 AM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `4jn21ai055_college`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+DROP TABLE IF EXISTS `class`;
+CREATE TABLE IF NOT EXISTS `class` (
+  `usn` varchar(30) NOT NULL,
+  `ssid` int NOT NULL,
+  PRIMARY KEY (`usn`,`ssid`),
+  KEY `ssid` (`ssid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`usn`, `ssid`) VALUES
+('4JN21AI001', 1),
+('4JN21AI002', 2),
+('4JN21AI003', 2),
+('4JN21AI004', 4),
+('4JN21AI005', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE IF NOT EXISTS `course` (
+  `subcode` varchar(50) NOT NULL,
+  `title` text NOT NULL,
+  `sem` int NOT NULL,
+  `credits` int NOT NULL,
+  PRIMARY KEY (`subcode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`subcode`, `title`, `sem`, `credits`) VALUES
+('21AI64', 'PAI', 6, 3),
+('21CS42', 'DSA', 4, 3),
+('21CS61', 'ATC', 6, 3),
+('21CS62', 'CN', 6, 5),
+('21CS63', 'DBMS', 6, 3),
+('21CS81', 'RI', 8, 3),
+('21CS82', 'NLP', 8, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `iam`
+-- (See below for the actual view)
+--
+DROP VIEW IF EXISTS `iam`;
+CREATE TABLE IF NOT EXISTS `iam` (
+`subcode` varchar(50)
+,`test1` int
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `iamarks`
+--
+
+DROP TABLE IF EXISTS `iamarks`;
+CREATE TABLE IF NOT EXISTS `iamarks` (
+  `usn` varchar(30) NOT NULL,
+  `subcode` varchar(50) NOT NULL,
+  `ssid` int NOT NULL,
+  `test1` int NOT NULL,
+  `test2` int NOT NULL,
+  `test3` int NOT NULL,
+  `finalia` int NOT NULL,
+  PRIMARY KEY (`usn`,`subcode`,`ssid`),
+  KEY `subcode` (`subcode`),
+  KEY `ssid` (`ssid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `iamarks`
+--
+
+INSERT INTO `iamarks` (`usn`, `subcode`, `ssid`, `test1`, `test2`, `test3`, `finalia`) VALUES
+('1BI15CS101', '21CS42', 5, 17, 20, 15, 19),
+('1BI15CS101', '21CS62', 4, 19, 20, 15, 20),
+('1BI15CS101', '21CS63', 5, 17, 19, 15, 18),
+('4JN21AI001', '21AI64', 1, 18, 14, 16, 17),
+('4JN21AI001', '21CS61', 1, 18, 13, 19, 19),
+('4JN21AI002', '21CS81', 2, 17, 13, 11, 15),
+('4JN21AI002', '21CS82', 2, 17, 15, 15, 16),
+('4JN21AI005', '21CS42', 4, 17, 20, 15, 19);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `semsec`
+--
+
+DROP TABLE IF EXISTS `semsec`;
+CREATE TABLE IF NOT EXISTS `semsec` (
+  `ssid` int NOT NULL,
+  `sem` int NOT NULL,
+  `sec` varchar(10) NOT NULL,
+  PRIMARY KEY (`ssid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `semsec`
+--
+
+INSERT INTO `semsec` (`ssid`, `sem`, `sec`) VALUES
+(1, 8, 'A'),
+(2, 8, 'A'),
+(3, 4, 'B'),
+(4, 4, 'C'),
+(5, 4, 'C');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE IF NOT EXISTS `student` (
+  `usn` varchar(30) NOT NULL,
+  `sname` text NOT NULL,
+  `address` text NOT NULL,
+  `phone` bigint NOT NULL,
+  `gender` text NOT NULL,
+  PRIMARY KEY (`usn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`usn`, `sname`, `address`, `phone`, `gender`) VALUES
+('1BI15CS101', 'Bora', 'Banglore', 9000910006, 'Male'),
+('4JN21AI001', 'Alia', 'JNNCE', 9000910001, 'Female'),
+('4JN21AI002', 'Sam', 'JNNCE', 9000910003, 'Male'),
+('4JN21AI003', 'Avin', 'JNNCE', 9000910003, 'Male'),
+('4JN21AI004', 'Romy', 'JNNCE', 9000910004, 'Female'),
+('4JN21AI005', 'Pommy', 'JNNCE', 9000910005, 'Female');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `iam`
+--
+DROP TABLE IF EXISTS `iam`;
+
+DROP VIEW IF EXISTS `iam`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`4JN21AI055`@`localhost` SQL SECURITY DEFINER VIEW `iam`  AS SELECT `i`.`subcode` AS `subcode`, `i`.`test1` AS `test1` FROM `iamarks` AS `i` WHERE (`i`.`usn` = '1BI15CS101') ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `class`
+--
+ALTER TABLE `class`
+  ADD CONSTRAINT `class_ibfk_1` FOREIGN KEY (`usn`) REFERENCES `student` (`usn`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `class_ibfk_2` FOREIGN KEY (`ssid`) REFERENCES `semsec` (`ssid`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `iamarks`
+--
+ALTER TABLE `iamarks`
+  ADD CONSTRAINT `iamarks_ibfk_1` FOREIGN KEY (`usn`) REFERENCES `student` (`usn`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `iamarks_ibfk_2` FOREIGN KEY (`subcode`) REFERENCES `course` (`subcode`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `iamarks_ibfk_3` FOREIGN KEY (`ssid`) REFERENCES `semsec` (`ssid`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ library.sql
@@ -0,0 +1,231 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 06, 2024 at 06:51 AM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `4jn21ai055_library`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `bnc`
+-- (See below for the actual view)
+--
+DROP VIEW IF EXISTS `bnc`;
+CREATE TABLE IF NOT EXISTS `bnc` (
+`bookid` int
+,`title` text
+,`sum(c.no_of_copies)` decimal(32,0)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE IF NOT EXISTS `book` (
+  `bookid` int NOT NULL,
+  `title` text NOT NULL,
+  `pub_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pub_year` int NOT NULL,
+  PRIMARY KEY (`bookid`),
+  KEY `pub_name` (`pub_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`bookid`, `title`, `pub_name`, `pub_year`) VALUES
+(1, 'Web Programming', 'PHI', 2003),
+(2, 'DBMS with SQL', 'PHI', 2002),
+(4, 'VHDL Programming', 'PHI', 2003),
+(5, 'Computer Networks and Security', 'DMH', 2018);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_authors`
+--
+
+DROP TABLE IF EXISTS `book_authors`;
+CREATE TABLE IF NOT EXISTS `book_authors` (
+  `bookid` int NOT NULL,
+  `author_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`bookid`,`author_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `book_authors`
+--
+
+INSERT INTO `book_authors` (`bookid`, `author_name`) VALUES
+(1, 'Raman Lamba'),
+(2, 'Elmasri'),
+(2, 'Navathe'),
+(4, 'Harris'),
+(5, 'Lakhan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_copies`
+--
+
+DROP TABLE IF EXISTS `book_copies`;
+CREATE TABLE IF NOT EXISTS `book_copies` (
+  `bookid` int NOT NULL,
+  `programme_id` int NOT NULL,
+  `no_of_copies` int NOT NULL,
+  PRIMARY KEY (`bookid`,`programme_id`),
+  KEY `book_copies_ibfk_2` (`programme_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `book_copies`
+--
+
+INSERT INTO `book_copies` (`bookid`, `programme_id`, `no_of_copies`) VALUES
+(1, 1, 10),
+(1, 2, 6),
+(2, 3, 8),
+(5, 1, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_lending`
+--
+
+DROP TABLE IF EXISTS `book_lending`;
+CREATE TABLE IF NOT EXISTS `book_lending` (
+  `bookid` int NOT NULL,
+  `programme_id` int NOT NULL,
+  `card_no` int NOT NULL,
+  `date_out` date NOT NULL,
+  `date_due` date NOT NULL,
+  PRIMARY KEY (`bookid`,`programme_id`,`card_no`,`date_out`),
+  KEY `book_lending_ibfk_2` (`programme_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `book_lending`
+--
+
+INSERT INTO `book_lending` (`bookid`, `programme_id`, `card_no`, `date_out`, `date_due`) VALUES
+(1, 1, 1, '2017-05-07', '2017-05-15'),
+(1, 3, 1, '2017-02-07', '2017-02-15'),
+(2, 2, 1, '2017-06-07', '2017-06-15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `library_programme`
+--
+
+DROP TABLE IF EXISTS `library_programme`;
+CREATE TABLE IF NOT EXISTS `library_programme` (
+  `programme_id` int NOT NULL,
+  `programme_name` text NOT NULL,
+  `address` text NOT NULL,
+  PRIMARY KEY (`programme_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `library_programme`
+--
+
+INSERT INTO `library_programme` (`programme_id`, `programme_name`, `address`) VALUES
+(1, 'CSE', 'CS BLOCK'),
+(2, 'ISE', 'ISE BLOCK'),
+(3, 'AIML', 'LIBRARY BLOCK'),
+(4, 'ECE', 'EC BLOCK'),
+(5, 'TCE', 'EC BLOCK');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publisher`
+--
+
+DROP TABLE IF EXISTS `publisher`;
+CREATE TABLE IF NOT EXISTS `publisher` (
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` bigint NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `publisher`
+--
+
+INSERT INTO `publisher` (`name`, `address`, `phone`) VALUES
+('DMH', 'Dharwad', 9900923001),
+('EEE', 'Bengaluru', 9900923002),
+('PHI', 'Mysuru', 9900923003),
+('Sapna', 'Bengaluru', 9900923004),
+('THM', 'Bengaluru', 9900923005);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `bnc`
+--
+DROP TABLE IF EXISTS `bnc`;
+
+DROP VIEW IF EXISTS `bnc`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`4JN21AI055`@`localhost` SQL SECURITY DEFINER VIEW `bnc`  AS SELECT `b`.`bookid` AS `bookid`, `b`.`title` AS `title`, sum(`c`.`no_of_copies`) AS `sum(c.no_of_copies)` FROM (`book` `b` join `book_copies` `c`) WHERE (`b`.`bookid` = `c`.`bookid`) GROUP BY `b`.`bookid` ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `book`
+--
+ALTER TABLE `book`
+  ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`pub_name`) REFERENCES `publisher` (`name`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `book_authors`
+--
+ALTER TABLE `book_authors`
+  ADD CONSTRAINT `book_authors_ibfk_1` FOREIGN KEY (`bookid`) REFERENCES `book` (`bookid`) ON DELETE CASCADE ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `book_copies`
+--
+ALTER TABLE `book_copies`
+  ADD CONSTRAINT `book_copies_ibfk_1` FOREIGN KEY (`bookid`) REFERENCES `book` (`bookid`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `book_copies_ibfk_2` FOREIGN KEY (`programme_id`) REFERENCES `library_programme` (`programme_id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `book_lending`
+--
+ALTER TABLE `book_lending`
+  ADD CONSTRAINT `book_lending_ibfk_1` FOREIGN KEY (`bookid`) REFERENCES `book` (`bookid`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `book_lending_ibfk_2` FOREIGN KEY (`programme_id`) REFERENCES `library_programme` (`programme_id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
