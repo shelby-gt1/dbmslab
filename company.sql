6fd9a87731981391deea9b51e0df54a31a076644
mrunknown <143517669+shelbygt1@users.noreply.github.com>
2024-03-21 17:11:02 +0530

    Update company.sql

--- company.sql
+++ company.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 06, 2024 at 06:51 AM
--- Server version: 8.2.0
--- PHP Version: 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -16,10 +8,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `4jn21ai055_company`
---
 
 -- --------------------------------------------------------
 
